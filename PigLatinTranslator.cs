c60cefdee28ca9f28d80f1bd5502ae8f1a603a05
kbdullah <kbdullah@gmail.com>
2016-06-17 23:25:03 -0500

    Initial addition of files and README
    Includes two .cs files - Program.cs (console application) and PigLatinTranslator.cs, and a README.txt

---
+++ PigLatinTranslator.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinTranslator
+{
+    public class PigLatinTranslator
+    {
+        char[] _vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+        public PigLatinTranslator()
+        {
+        }
+
+        public String PigLatinTranslate(String phrase)
+        {
+            // take in full string of user input
+            String[] words = phrase.Split(' ');
+            String translatedPhrase = String.Empty;
+
+            foreach(var word in words)
+            {
+                // does the word have letters?
+                var hasLetters = word.Any(c => char.IsLetter(c));
+                // set the translated word to the same word by default
+                var translatedWord = word;
+                // if the word has letters, translate the word to pig latin
+                if(hasLetters)
+                {
+                    translatedWord = SingleWordPigLatinTranslate(word);
+                }
+                translatedPhrase += translatedWord;
+                translatedPhrase += " ";
+            }
+
+            // remove trailing whitespace
+            translatedPhrase = translatedPhrase.TrimEnd(' ');
+
+            return translatedPhrase;
+        }
+
+        private String SingleWordPigLatinTranslate(String word)
+        {
+            String puncSubStr;
+            word = SeparatePunctuation(word, out puncSubStr);
+            // does the word begin with a capital letter?
+            // find the first character of the word
+            var firstChar = word.First();
+            var firstCharUpper = char.IsUpper(firstChar);
+            // lower case the word
+            word = word.ToLower();
+            // does the word contain consonants
+            var containsConsonants = ContainsConsonants(word);
+            // default to "no consonants" standard where prefix is empty and word is stem
+            String prefix = String.Empty;
+            String stem = word;
+            String suffix = "yay";
+            // if we contain consonants, separate the word into the prefix and stem
+            if (containsConsonants)
+            {
+                // find the index of the first vowel
+                var firstVowel = word.First(c => _vowels.Contains(c));
+                var indexOfFirstVowel = word.IndexOf(firstVowel);
+                // take the substring to find the prefix
+                prefix = word.Substring(0, indexOfFirstVowel);
+                // take the substring to find the stem
+                stem = word.Substring(indexOfFirstVowel, word.Length - prefix.Length);
+                // set the suffix to "ay"
+                suffix = "ay";
+            }
+            // reverse the order of the string and the prefix, then append pig latin suffix ay/yay, and last add punctuation
+            var translation = stem + prefix + suffix + puncSubStr;
+            // if the first letter was capitalized, then capitalize the first letter of the translation
+            if (firstCharUpper)
+            {
+                translation = CapitalizeFirstCharacter(translation);
+            }
+
+            return translation;
+        }
+
+        private bool ContainsConsonants(String word)
+        {
+            var consonantFlag = false;
+            var lowerWord = word.ToLower();
+
+            foreach (char c in lowerWord)
+            {
+                if (!_vowels.Contains(c))
+                {
+                    consonantFlag = true;
+                    break;
+                }
+            }
+
+            return consonantFlag;
+        }
+
+        private String CapitalizeFirstCharacter(String word)
+        {
+            // identify the first character
+            var firstChar = word[0];
+            // separate the rest of the word from the first character
+            var suffix = word.Substring(1, word.Length - 1);
+            // upper case the first character
+            firstChar = char.ToUpper(firstChar);
+            // add back the rest of the word to the first character
+            word = firstChar + suffix;
+
+            return word;
+        }
+
+        /// <summary>
+        /// Returns any terminating punctuation. Otherwise, returns String.Empty
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        private String SeparatePunctuation(String word, out String puncSubStr)
+        {
+            puncSubStr = String.Empty;
+            // is there any punctuation? 
+            var anyPunc = word.Any(c => char.IsPunctuation(c));
+            if (anyPunc)
+            {
+                // isolate any leading/trailing punctuation
+                var firstPunc = word.First(c => char.IsPunctuation(c));
+                // find the index of the punctuation
+                var indexOfFirstPunc = word.IndexOf(firstPunc);
+                // isolate the punctuation and anything after it
+                puncSubStr = word.Substring(indexOfFirstPunc);
+                // 
+                word = word.Substring(0, indexOfFirstPunc);
+            }
+
+            return word;
+        }
+    }
+
+    
+}
---
+++ README.txt
@@ -0,0 +1,38 @@
+﻿Author: Khadir Abdullah
+Date: 6/17/2016
+
+This is the Pig Latin Translator, a console application which translates user input into pig latin translations.
+
+The program takes in user a line of user input and returns the pig latin translation. Users can exit the program by
+typing 'exit'. The 'exit' command is case-sensitive.
+
+ARCHITECTURE
+
+Most of the work within the program occurs within the PigLatinTranslator class. The PigLatinTranslator class has two
+main functions, PigLatinTranslate and SingleWordPigLatinTranslate. PigLatinTranslate translates the users input phrase
+by iterating over the words in the phrase, calling SingleWordPigLatinTranslate. SingleWordPigLatinTranslate
+utilizes multiple helper functions SeparatePunctuation, ContainsConsonant, and CapitalizeFirstCharacter to meet
+the given requirements. After separating the punctuation, SWPLT substrings the prefix and the stem, and then appends 
+the appropriate suffix (based on the result of ContainsConsonant) as well as the punctuation. Then, the first letter
+is capitalized if needed be.
+
+ASSUMPTIONS
+I'm assuming all input is valid, as per the requirements. As of 6/17/2016, there is no input validation 
+(although that could change this weekend if I get the time).
+A valid input is an input that is input made up of characters, numbers, spaces, and the {, ! ?} punctuations. 
+
+INSTANCES WHERE THE SAMPLE OUTPUT VARIED FROM THE REQUIREMENTS
+
+I also noticed that two lines in the sample output were inconsistent with the requirements:
+
+Per the requirements, "eosshay" below should be "oesshay", however the sample output (see below) has it spelled
+differently. Also, "uddybah" in the second sample output should be "uddybay". I assumed that both of these were 
+typos instead of the correct output (which would mean the requirements were incorrect). As such, the 
+PigLatinTranslator adheres to the stated requirements, not the sample output.
+
+--> No shirts, no shoes, no service
+Onay irtsshay, onay eosshay, onay ervicesay
+
+--> Hey buddy, get away from my car!
+Eyhay uddybah, etgay awayay omfray ymay arcay!
+
